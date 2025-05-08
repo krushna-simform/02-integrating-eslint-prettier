03f1347be2cbf319017db8c255ce44605565c654
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-08 14:16:04 +0530

    FEATURE: intergate eslint and prettier in vite + react project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+    return (
+        <div className="container">
+            <p>Hello, my name is Krushna Rathod.</p>
+            <p>
+                My email is:
+                <a href="mailto:krushna.rathod@simformsolutions.com">
+                    krushna.rathod@simformsolutions.com
+                </a>
+            </p>
+            <p>
+                My GitHub account:
+                <a href="https://github.com/krushna-simform">
+                    github.com/krushna-simform
+                </a>
+            </p>
+        </div>
+    );
+}
+
+export default App;
